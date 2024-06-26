635c3c56aaf55386471cf474f501402b2b852e93
sreenathg <sreenath.pg3333@gmail.com>
2024-06-27 00:19:05 +0530

    add support for switching camera

--- script.js
+++ script.js
@@ -1,74 +1,139 @@
 (() => {
   const width = 320;
   let height = 0;
-   let video = null;
-   let canvas= null;
-  let captureBtn ;
+  let video = null;
+  let canvas = null;
+  let captureBtn;
   let photo;
   let select;
-  function startup(){
+  let stopBtn;
+  let selectedDeviceId;
+  const defaultConstraint = {
+    video: true,
+    audio: false,
+  };
+  function startup() {
     video = document.querySelector("#video");
     canvas = document.querySelector("#canvas");
     captureBtn = document.querySelector("#capture");
-    photo= document.querySelector("#photo");
+    // photo = document.querySelector("#photo");
     select = document.querySelector("#deviceSelection");
+    stopBtn = document.querySelector("#stopBtn");
 
     navigator.mediaDevices
-    .enumerateDevices()
-    .then((devices) => {
-      devices.forEach((device) => {
-        let option = new Option(`${device.kind}: ${device.label}`,device.deviceId)
-        select.add(option)
-        console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
+      .enumerateDevices()
+      .then((devices) => {
+        //filter(d => d.kind === 'videoinput')
+        devices.forEach((device) => {
+          let option = new Option(
+            `${device.kind}: ${device.label}`,
+            device.deviceId
+          );
+          select.add(option);
+          console.log(
+            `${device.kind}: ${device.label} id = ${device.deviceId}`
+          );
+        });
+      })
+      .catch((err) => {
+        console.error(`${err.name}: ${err.message}`);
       });
-    })
-    .catch((err) => {
-      console.error(`${err.name}: ${err.message}`);
+    console.log(select.value);
+    startStream();
+
+    video.addEventListener(
+      "canplay",
+      (event) => {
+        height = video.videoHeight / (video.videoWidth / width);
+        canvas.setAttribute("width", width);
+        canvas.setAttribute("height", height);
+      },
+      false
+    );
+
+    captureBtn.addEventListener(
+      "click",
+      (event) => {
+        event.preventDefault();
+        takePicture();
+      },
+      false
+    );
+
+    select.addEventListener("change", function () {
+      // This function will execute whenever an option is selected
+      let selectedOption = select.options[select.selectedIndex];
+      let selectedValue = selectedOption.value;
+      stopStream();
+      console.log("Selected value:", selectedValue);
+
+      startStream();
+      // You can perform any actions based on the selected value here
     });
 
-    navigator.mediaDevices.getUserMedia({
-      video:true,
-      audio: false
-    }).then(stream => {
-      video.srcObject = stream;
-      video.play()
-    }).catch(err => {
-      console.log(`some error occured`, err)
-    })
-
-    video.addEventListener('canplay', (event) => {
-      height = video.videoHeight / (video.videoWidth/width);
-      canvas.setAttribute('width', width);
-      canvas.setAttribute("height", height);
-    }, false)
-
-    captureBtn.addEventListener('click', (event) => {
-      event.preventDefault();
-      takePicture()
-    }, false)
+    stopBtn.addEventListener(
+      "click",
+      (event) => {
+        event.preventDefault();
+        stopStream();
+      },
+      false
+    );
   }
+
+  function startStream() {
+    navigator.mediaDevices
+      .getUserMedia({
+        ...defaultConstraint,
+        deviceId: { exact: select.value },
+      })
+      .then((stream) => {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.log(`some error occured`, err);
+      });
+  }
+
+
+  function stopStream() {
+    if (video && video.srcObject) {
+      const stream = video.srcObject;
+      const tracks = stream.getTracks();
+
+      tracks.forEach((track) => {
+        track.stop();
+      });
+
+      video.srcObject = null;
+    }
+  }
+
+
   function clearphoto() {
     const context = canvas.getContext("2d");
     context.fillStyle = "#AAA";
     context.fillRect(0, 0, canvas.width, canvas.height);
-  
+
     const data = canvas.toDataURL("image/png");
-    photo.setAttribute("src", data);
+    // photo.setAttribute("src", data);
   }
+
+
   function takePicture() {
     const context = canvas.getContext("2d");
     if (width && height) {
       canvas.width = width;
       canvas.height = height;
       context.drawImage(video, 0, 0, width, height);
-  
+
       const data = canvas.toDataURL("image/png");
       photo.setAttribute("src", data);
     } else {
       clearphoto();
     }
   }
-  
-  
-   window.addEventListener("load", startup, false)
-  })()
+
+  window.addEventListener("load", startup, false);
+})();
